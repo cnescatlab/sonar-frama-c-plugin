225dd5754e9700052c1d1739b958b06197b50213
Cyrille Francois <francois.cyrille@gmail.com>
2017-08-31 20:23:48 +0200

    Add first unitary test

---
+++ src/test/java/fr/cnes/sonarqube/plugins/framac/FramaCPluginTest.java
@@ -0,0 +1,23 @@
+package fr.cnes.sonarqube.plugins.framac;
+
+import org.junit.Test;
+import org.sonar.api.Plugin;
+
+import fr.cnes.sonarqube.plugins.framac.languages.FramaCLanguage;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class FramaCPluginTest {
+
+	@Test
+	public void testExtension() {
+		
+		Plugin.Context context = mock(Plugin.Context.class);
+		FramaCPlugin plugin = new FramaCPlugin();
+		plugin.define(context);
+		
+		verify(context).addExtension(FramaCLanguage.class);
+	}
+
+}
