409f54b6a54f4e37934cb9c78269221a0550f23e
CfrancCyrille <francois.cyrille@gmail.com>
2017-11-24 01:34:09 +0100

    New frama-c dashboard with bullet charts

--- src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
+++ src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
@@ -1,7 +1,6 @@
-import "./MetricsSummaryTab.css";
-import MetricsSummary from '../MetricsSummary';
-import MetricsBulletChart from '../MetricsBulletChart';
 import React from "react";
+import "./MetricsSummaryTab.css";
+import MetricsSummaryPanel from '../MetricsSummaryPanel';
 
 class MetricsSummaryTab extends React.Component {
 
@@ -13,41 +12,21 @@ class MetricsSummaryTab extends React.Component {
 
         this.setState({
             data: [
-                { name: 'Nesting', total: '-', min: '-', mean: '-', max: '-' },
-                { name: 'Ratio Comment', total: '-', min: '-', mean: '-', max: '-' },
                 { name: 'Complexity Simplified', total: '-', min: 3, mean: 3.5, max: 4 },
-                { name: 'Line Of Code', total: 27, min: 12, mean: 13.5, max: 15 }
+                { name: 'Line Of Code', total: 35, min: 12, mean: 13.5, max: 15 }
             ]
         });
     }
 
     render() {
         return (
-        	    <div className="App">
+            <div className = "MetricsSummaryTab" >
 
-        	    <table>
-        	      <tr>
-        	      <th></th>
-        	      <th>Total</th>
-        	      <th>Min</th>
-        	      <th>Mean</th>
-        	      <th>Max</th>
-        	      </tr>
-        	      <tbody>
-        	        {this.state.data.map(
-        	            (item) =>
-        	            <MetricsSummary
-        	                item={item}
-        	              />
-        	          )
-        	        }
-        	        </tbody>
-        	    </table>
-        	    <MetricsBulletChart/>
-        	        </div>
-        	 
-        		);
-    }
+                <MetricsSummaryPanel label='' data={this.state.data}/>
+
+            </div>
+        );
+    };
 }
 
 export default MetricsSummaryTab;
