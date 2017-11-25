dc2b7bf316c957ace833785873da68508f817123
Cyrille Francois <francois.cyrille@gmail.com>
2017-11-25 21:56:42 +0100

    Sonar API access for dashboard

--- src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
+++ src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
@@ -1,40 +1,42 @@
 import React from "react";
 import "./MetricsSummaryTab.css";
 import MetricsSummaryPanel from '../MetricsSummaryPanel';
+import {getJSON} from 'sonar-request';
 
 class MetricsSummaryTab extends React.Component {
+	
+	const metricKeys = ['framac-cyclomatic-complexity-min','framac-cyclomatic-complexity-mean','framac-cyclomatic-complexity-max','framac-sloc','framac-sloc-min','framac-sloc-mean','framac-sloc-max'];
 
     state = {
         data: []
     };
 
-      findMeasures() {
+    findMeasures() {
 /*     fetch('http://192.168.1.25:9000/api/measures/component?metricKeys=framac-sloc-mean&component=fr.cnes.framac:framac-metrics-sq-scanner').then(function (response) {
       console.log(response.measures);
     return response.measures;
    }); */
 
-    return getJSON('/api/measures/component', {
-        metricKeys:'framac-sloc-mean',
-        component:'fr.cnes.framac:framac-metrics-sq-scanner'
-      }).then(function (response) {
-        console.log(response.measures);
-      return response.measures;
-   });
-  }
+    	return getJSON('/api/measures/component', {
+    		metricKeys:'framac-sloc-mean',
+    		component:'fr.cnes.framac:framac-metrics-sq-scanner'
+    	}).then(function (response) {
+    		return response.component.measures;
+    	});
+    }
 
-  constructor(){
-    super();
-    this.findMeasures = this.findMeasures.bind(this);
-  }
+    constructor(){
+    	super();
+    	this.findMeasures = this.findMeasures.bind(this);
+    }
 
-  componentDidMount() {
+    componentDidMount() {
         
-  this.findMeasures().then((valuesReturnedByAPI) => {
-    this.setState({
-      data: valuesReturnedByAPI
-    });
-  });
+    	this.findMeasures().then((valuesReturnedByAPI) => {
+    		this.setState({
+    			data: valuesReturnedByAPI
+    		});
+    	});
 
         this.setState({
             data: [
