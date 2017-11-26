a494e0ef2da335628e8593d181c7cadcd158092b
Cyrille Francois <francois.cyrille@gmail.com>
2017-11-26 08:09:13 +0100

    Connect dashboard to the Sonar web server

--- src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
+++ src/main/js/components/MetricsSummaryTab/MetricsSummaryTab.js
@@ -3,47 +3,90 @@ import "./MetricsSummaryTab.css";
 import MetricsSummaryPanel from '../MetricsSummaryPanel';
 import {getJSON} from 'sonar-request';
 
+const metricKeysTab = [
+	'framac-cyclomatic-complexity-min',
+	'framac-cyclomatic-complexity-mean',
+	'framac-cyclomatic-complexity-max',
+	'framac-sloc',
+	'framac-sloc-min',
+	'framac-sloc-mean',
+	'framac-sloc-max'];
+
+function findMeasures(componentName) {
+	return new Promise(function(resolve, reject) {
+		
+	let metricKeys = metricKeysTab.reduce((accumulator, currentValue) => accumulator + ',' + currentValue);
+
+	let measure_component = getJSON('/api/measures/component', {
+		metricKeys:metricKeys,
+		component:componentName
+	}).then(function (measure_component) {
+		console.log(measure_component);
+		
+		// Build map 'metric:value'
+		let allMetrics = new Map();
+		measure_component.component.measures.forEach(element => {
+		    for (let index = 0; index < metricKeysTab.length; index++) {
+		      const key = metricKeysTab[index];
+		      if(element.metric === key){
+		    	  allMetrics.set(element.metric, element.value);
+		      }      
+		    }
+		});
+		console.log(allMetrics);
+		
+		// Build result table
+		let res=[
+/*			{name:'Nesting', total: '-', min: '-', mean: '-', max: '-'},
+			{name:'Ratio Comment', total: '-', min: '-', mean: '-', max: '-'},
+*/			{name:'Complexity Simplified', 
+				total: '-', 
+				min: allMetrics.get('framac-cyclomatic-complexity-min'), 
+				mean: allMetrics.get('framac-cyclomatic-complexity-mean'), 
+				max: allMetrics.get('framac-cyclomatic-complexity-max')
+			},
+			{name:'Line Of Code', 
+				total: allMetrics.get('framac-sloc'),
+				min: allMetrics.get('framac-sloc-min'), 
+				mean: allMetrics.get('framac-sloc-mean'), 
+				max: allMetrics.get('framac-sloc-max')
+			}
+		];	
+		console.log(res);
+
+		resolve(res);
+	}).catch(function(error) {
+	    console.log('No measures found: ' + error.message); 
+	    reject(null);
+	});
+});
+}
+
 class MetricsSummaryTab extends React.Component {
 	
-	const metricKeys = ['framac-cyclomatic-complexity-min','framac-cyclomatic-complexity-mean','framac-cyclomatic-complexity-max','framac-sloc','framac-sloc-min','framac-sloc-mean','framac-sloc-max'];
-
     state = {
         data: []
     };
 
-    findMeasures() {
-/*     fetch('http://192.168.1.25:9000/api/measures/component?metricKeys=framac-sloc-mean&component=fr.cnes.framac:framac-metrics-sq-scanner').then(function (response) {
-      console.log(response.measures);
-    return response.measures;
-   }); */
-
-    	return getJSON('/api/measures/component', {
-    		metricKeys:'framac-sloc-mean',
-    		component:'fr.cnes.framac:framac-metrics-sq-scanner'
-    	}).then(function (response) {
-    		return response.component.measures;
-    	});
-    }
-
     constructor(){
     	super();
-    	this.findMeasures = this.findMeasures.bind(this);
     }
 
     componentDidMount() {
-        
-    	this.findMeasures().then((valuesReturnedByAPI) => {
-    		this.setState({
-    			data: valuesReturnedByAPI
-    		});
+    	console.log(this.props);
+        console.log('MetricsSummaryTab:'+this.props.project);
+    	findMeasures(this.props.project.key).then((item) => {
+    	    this.setState({
+    	      data: item
+    	    });
     	});
 
-        this.setState({
+/*        this.setState({
             data: [
                 { name: 'Complexity Simplified', total: '-', min: 3, mean: 3.5, max: 4 },
                 { name: 'Line Of Code', total: 35, min: 12, mean: 13.5, max: 15 }
             ]
-        });
+        });*/
     }
 
     render() {
